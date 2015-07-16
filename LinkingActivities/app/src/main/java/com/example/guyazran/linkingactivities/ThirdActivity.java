f67264d3c58e7dcd52958f3f4cfe55c634962a6f
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-07-16 15:21:27 +0300

    Linking Activities

---
+++ LinkingActivities/app/src/main/java/com/example/guyazran/linkingactivities/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.guyazran.linkingactivities;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by guyazran on 7/16/15.
+ */
+public class ThirdActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third);
+    }
+}
