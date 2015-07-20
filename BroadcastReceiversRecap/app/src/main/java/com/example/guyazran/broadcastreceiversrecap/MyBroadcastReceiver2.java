88285ffd338d59c4ac353930c64c37cd58ca94a5
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-07-20 11:28:02 +0300

    Broadcast Receivers Recap

---
+++ BroadcastReceiversRecap/app/src/main/java/com/example/guyazran/broadcastreceiversrecap/MyBroadcastReceiver2.java
@@ -0,0 +1,18 @@
+package com.example.guyazran.broadcastreceiversrecap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by guyazran on 7/20/15.
+ */
+public class MyBroadcastReceiver2 extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String s = "Broadcast Received in MyBroadcastReceiver2: " + intent.getStringExtra("key");
+        Toast.makeText(context, s, Toast.LENGTH_LONG).show();
+        abortBroadcast();
+    }
+}
---
+++ BroadcastReceiversRecap/app/src/main/java/com/example/guyazran/broadcastreceiversrecap/MyBroadcastReceiver3.java
@@ -0,0 +1,17 @@
+package com.example.guyazran.broadcastreceiversrecap;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by guyazran on 7/20/15.
+ */
+public class MyBroadcastReceiver3 extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String s = "Broadcast Received in MyBroadcastReceiver3: " + intent.getStringExtra("key");
+        Toast.makeText(context, s, Toast.LENGTH_LONG).show();
+    }
+}
