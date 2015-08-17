75d1a075d8b2df0a0db72ed66449d0625536ca5f
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-08-17 11:40:52 +0300

    Sending SMS Using Built-in Messenger

---
+++ SendingSMSUsingBuiltInApp/app/src/main/java/com/example/guyazran/sendingsmsusingbuiltinapp/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.guyazran.sendingsmsusingbuiltinapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.putExtra("address", "0527800169");
+        intent.putExtra("sms_body", "That's the message!");
+        intent.setType("vnd.android-dir/mms-sms");
+        startActivity(intent);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
