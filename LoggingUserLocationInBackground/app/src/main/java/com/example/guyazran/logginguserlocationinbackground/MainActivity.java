f6123ec7141d18c3676efc4099aceef473223ae6
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-08-31 10:03:10 +0300

    Reading From A Database

--- LoggingUserLocationInBackground/app/src/main/java/com/example/guyazran/logginguserlocationinbackground/MainActivity.java
+++ LoggingUserLocationInBackground/app/src/main/java/com/example/guyazran/logginguserlocationinbackground/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.guyazran.logginguserlocationinbackground;
 
+import android.database.Cursor;
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +18,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         DBAdapter dbAdapter = new DBAdapter(this);
+        exampleOfWritingToDB(dbAdapter);
+        exampleOfReadingFromDB(dbAdapter);
+    }
+
+    void exampleOfWritingToDB(DBAdapter dbAdapter){
         try {
             dbAdapter.open();
-            dbAdapter.insertLocation(123,456);
+            dbAdapter.insertLocation(123, 456);
+            dbAdapter.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    void exampleOfReadingFromDB(DBAdapter dbAdapter){
+        try {
+            dbAdapter.open();
+            Cursor cursor = dbAdapter.getAllLocations();
+            while (cursor.moveToNext()){
+                long time = cursor.getLong(0);
+                double lat = cursor.getDouble(1);
+                double lng = cursor.getDouble(cursor.getColumnIndex(dbAdapter.KEY_LNG));
+                Log.d("Guy", "location time=" + time + " lat=" + lat + " lng=" + lng);
+            }
+            cursor.close();
             dbAdapter.close();
         } catch (SQLException e) {
             e.printStackTrace();
