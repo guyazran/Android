237f3aa59afefa9ed4e089269a540f9992617f00
Guy Azran <guyazran@Guys-MacBook-Pro.local>
2015-09-03 14:25:15 +0300

    Reading And Writing Files To Cache Directory

--- SavingFilesToTheDataDirectory/app/src/main/java/com/example/guyazran/savingfilestothedatadirectory/MainActivity.java
+++ SavingFilesToTheDataDirectory/app/src/main/java/com/example/guyazran/savingfilestothedatadirectory/MainActivity.java
@@ -7,6 +7,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -21,9 +22,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        File cacheDir = getCacheDir();
+        File file = new File(cacheDir, "textfile.txt");
+
         //writing to file (creates file if doesn't exist or overwrites the existing file)
         try {
-            FileOutputStream fileOutputStream = openFileOutput("textfile.txt", MODE_PRIVATE);
+            //FileOutputStream fileOutputStream = openFileOutput("textfile.txt", MODE_PRIVATE);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fileOutputStream);
             outputStreamWriter.write("again, we are learning today about writing to files.");
             outputStreamWriter.close();
@@ -37,10 +43,13 @@ public class MainActivity extends AppCompatActivity {
 
         //reading from file
         try {
-            FileInputStream fileInputStream = openFileInput("textfile.txt");
+            //FileInputStream fileInputStream = openFileInput("textfile.txt");
+            FileInputStream fileInputStream = new FileInputStream(file);
             InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream);
             char[] chars = new char[1024];
             int actuallyRead = inputStreamReader.read(chars);
+            inputStreamReader.close();
+            fileInputStream.close();
             String s = new String(chars, 0, actuallyRead);
             Toast.makeText(MainActivity.this, "S=" + s, Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
